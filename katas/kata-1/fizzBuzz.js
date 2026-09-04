2ebfb3950a8807562223e431e8b25b9c1d0201f3
FrstF4ll <tizdimo@gmail.com>
2026-09-04 15:54:14 +0200

    feat: print Buzz for multiple of five

--- katas/kata-1/fizzBuzz.js
+++ katas/kata-1/fizzBuzz.js
@@ -2,5 +2,8 @@ export const fizzBuzz = (number) => {
     if(number % 3 === 0){
         return "Fizz"
     } 
+    if(number % 5 === 0){
+        return "Buzz"
+    }
     return number.toString()
 }
