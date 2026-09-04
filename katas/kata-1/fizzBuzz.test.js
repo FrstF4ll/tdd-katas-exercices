535a24d6e2de8d0589e43d3e54d3286a67a6ea28
FrstF4ll <tizdimo@gmail.com>
2026-09-04 15:49:28 +0200

    test: build multiple of 3 test

--- katas/kata-1/fizzBuzz.test.js
+++ katas/kata-1/fizzBuzz.test.js
@@ -3,3 +3,8 @@ import { fizzBuzz } from './fizzBuzz';
 test('return "1" for 1',() => {
     expect(fizzBuzz(1)).toBe("1")
 })
+
+
+test('is returning "Fizz" for multiple of 3', () => {
+    expect(fizzBuzz(3)).toBe("Fizz")
+});
