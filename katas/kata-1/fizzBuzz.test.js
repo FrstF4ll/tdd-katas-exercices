cd0f4cc05576faa0e264eeedafe429289fb55f21
FrstF4ll <tizdimo@gmail.com>
2026-09-04 15:54:02 +0200

    test: build check for multiple of 5

--- katas/kata-1/fizzBuzz.test.js
+++ katas/kata-1/fizzBuzz.test.js
@@ -8,3 +8,7 @@ test('return "1" for 1',() => {
 test('is returning "Fizz" for multiple of 3', () => {
     expect(fizzBuzz(3)).toBe("Fizz")
 });
+
+test('is returning "Buzz" for multiple of 5', () => {
+    expect(fizzBuzz(5)).toBe("Buzz")
+});
