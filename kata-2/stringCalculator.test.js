cc5df01a6d84677cfb18d1025b8e9eda2e2c45d4
FrstF4ll <tizdimo@gmail.com>
2026-09-04 16:47:24 +0200

    test: build test for sum of selected number

--- kata-2/stringCalculator.test.js
+++ kata-2/stringCalculator.test.js
@@ -4,3 +4,8 @@ import { Add } from './stringCalculator'
 test('Empty strings return 0', () => {
     expect(Add("")).toBe(0)
 })
+
+test("Return sum of selected numbers", () =>{
+    expect(Add("1,2")).toBe(3)
+    expect(Add(",3")).toBe(3)
+})
