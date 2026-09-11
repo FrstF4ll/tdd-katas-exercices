7a1b695ef815b5fc30bf86563a6d913cb984343b
FrstF4ll <tizdimo@gmail.com>
2026-09-11 09:49:37 +0200

    feat: implement custom delimiters

--- kata-2/stringCalculator.js
+++ kata-2/stringCalculator.js
@@ -1,7 +1,21 @@
 export const Add = (numbers) => {
-    const separators = /[,\n]/
+    const definedPattern = /^\/\/(.+)\n([\s\S]*)$/
+    const matched = numbers.match(definedPattern)
+    let separators = /[,\n]/
+
+    if(matched){
+        const delimiter = matched[1]
+        const parsedNumbers = matched[2]
+        const parsedSplittedNumbers = parsedNumbers.split(delimiter)
+        const mappedParsed = parsedSplittedNumbers.map(Number)
+        const parsedFiltered = mappedParsed.filter((number) => number < 1000)
+        return parsedFiltered.reduce((acc, curr) => acc + curr,0)
+    } else {
+
     const numArray = numbers.split(separators);
     const mapped = numArray.map(Number);
     const filtered =  mapped.filter((number) => number < 1000)
     return filtered.reduce((acc, curr) => acc + curr,0)
+    }
 }
+
