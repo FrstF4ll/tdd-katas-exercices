9917909d613bf0d5a279e2891332a486f75b3803
FrstF4ll <tizdimo@gmail.com>
2026-09-11 13:42:30 +0200

    refactor: replace var for convenient variable declaration keywords

--- kata-3/TennisGame1.js
+++ kata-3/TennisGame1.js
@@ -16,54 +16,54 @@ class TennisGame1 {
 
 
 
-getScore() {
-    var score = "";
-    var tempScore = 0;
-    if (this.m_score1 === this.m_score2) {
-        switch (this.m_score1) {
-            case 0:
-                score = "Love-All";
-                break;
-            case 1:
-                score = "Fifteen-All";
-                break;
-            case 2:
-                score = "Thirty-All";
-                break;
-            default:
-                score = "Deuce";
-                break;
-        }
-    } else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
-        var minusResult = this.m_score1 - this.m_score2;
-        if (minusResult === 1) score = "Advantage player1";
-        else if (minusResult === -1) score = "Advantage player2";
-        else if (minusResult >= 2) score = "Win for player1";
-        else score = "Win for player2";
-    } else {
-        for (var i = 1; i < 3; i++) {
-            if (i === 1) tempScore = this.m_score1;
-            else {
-                score += "-";
-                tempScore = this.m_score2;
-            }
-            switch (tempScore) {
+    getScore() {
+        let score = "";
+        let tempScore = 0;
+        if (this.m_score1 === this.m_score2) {
+            switch (this.m_score1) {
                 case 0:
-                    score += "Love";
+                    score = "Love-All";
                     break;
                 case 1:
-                    score += "Fifteen";
+                    score = "Fifteen-All";
                     break;
                 case 2:
-                    score += "Thirty";
+                    score = "Thirty-All";
                     break;
-                case 3:
-                    score += "Forty";
+                default:
+                    score = "Deuce";
                     break;
             }
+        } else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
+            const minusResult = this.m_score1 - this.m_score2;
+            if (minusResult === 1) score = "Advantage player1";
+            else if (minusResult === -1) score = "Advantage player2";
+            else if (minusResult >= 2) score = "Win for player1";
+            else score = "Win for player2";
+        } else {
+            for (let i = 1; i < 3; i++) {
+                if (i === 1) tempScore = this.m_score1;
+                else {
+                    score += "-";
+                    tempScore = this.m_score2;
+                }
+                switch (tempScore) {
+                    case 0:
+                        score += "Love";
+                        break;
+                    case 1:
+                        score += "Fifteen";
+                        break;
+                    case 2:
+                        score += "Thirty";
+                        break;
+                    case 3:
+                        score += "Forty";
+                        break;
+                }
+            }
         }
-    }
-    return score;
+        return score;
     };
 }
 if (typeof window === "undefined") {
