c45660b78c3d7fba5d382a66f27a947889f5b26e
FrstF4ll <tizdimo@gmail.com>
2026-09-11 08:52:45 +0200

    feat: implement separator regex

--- kata-2/stringCalculator.js
+++ kata-2/stringCalculator.js
@@ -1,5 +1,6 @@
 export const Add = (numbers) => {
-    const numArray = numbers.split(",");
+    const separators = /[,\n]/
+    const numArray = numbers.split(separators);
     const mapped = numArray.map(Number);
     const filtered =  mapped.filter((number) => number < 1000)
     return filtered.reduce((acc, curr) => acc + curr,0)
