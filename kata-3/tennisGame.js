5438ce2c69d0af90f66c0c0a50709d820084416b
FrstF4ll <207869736+FrstF4ll@users.noreply.github.com>
2026-09-18 09:23:36 +0200

    refactor: extract winning logic in a function

--- kata-3/tennisGame.js
+++ kata-3/tennisGame.js
@@ -1,10 +1,11 @@
-export function tennisGame(player1Score, player2Score){
+function winningPlayer(player1Score, player2Score){
     const scoreDiff = player1Score - player2Score
-    let resultMessage = ''
     if(player1Score >= 4 && scoreDiff >= 2){
-        resultMessage = "Win for player 1"
+        return "Win for player 1"
     } else if(player2Score >= 4 && scoreDiff <= -2){
-        resultMessage =  "Win for player 2"
+        return "Win for player 2"
     }
-    return resultMessage
+}
+export function tennisGame(player1Score, player2Score){
+    return winningPlayer(player1Score, player2Score)
 }
