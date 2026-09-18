0a64abc6352f0c6ba0fdd3494dc7b14febf36b0d
FrstF4ll <207869736+FrstF4ll@users.noreply.github.com>
2026-09-18 11:52:55 +0200

    test: build test for equivalent results

--- kata-3/tennisGame.test.js
+++ kata-3/tennisGame.test.js
@@ -38,3 +38,14 @@ describe("Display score diff based labels", () => {
         expect(tennisGame(player_1, player_2)).toBe(label)
     })
 })
+
+describe("Display same score based label", () => {
+    test.each([
+        [0,0, "Love-Love"],
+        [1,1, "Fifteen-Fifteen"],
+        [4,4, "Deuce"],
+        [5,5, "Deuce"]
+    ])("%(i, %i) -> %s", (player_1, player_2, label) => {
+        expect(tennisGame(player_1, player_2)).toBe(label)
+    })
+})
