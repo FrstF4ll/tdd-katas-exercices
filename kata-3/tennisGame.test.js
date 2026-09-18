118df1321f54997cd4a62734f976e93dbfc56231
FrstF4ll <207869736+FrstF4ll@users.noreply.github.com>
2026-09-18 08:47:59 +0200

    test: build win condition tests

---
+++ kata-3/tennisGame.test.js
@@ -0,0 +1,8 @@
+import {test, expect} from "vitest";
+
+test("Win conditions are correctly defined", () => {
+    expect(tennisGame(4, 0)).toBe("Win for player 1")
+    expect(tennisGame(0, 4)).toBe("Win for player 2")
+    expect(tennisGame(6, 4)).toBe("Win for player 1")
+    expect(tennisGame(4, 6)).toBe("Win for player 2")
+})
