85f9605ddc7caa7c1c96965a591de5aab75789d4
FrstF4ll <207869736+FrstF4ll@users.noreply.github.com>
2026-09-18 11:18:01 +0200

    test: build tests for early-game labels

--- kata-3/tennisGame.test.js
+++ kata-3/tennisGame.test.js
@@ -1,4 +1,4 @@
-import {test, expect, } from "vitest";
+import {test, expect, describe } from "vitest";
 import {tennisGame} from "./tennisGame";
 test("Display winning player", () => {
     expect(tennisGame(4, 0)).toBe("Win for player 1")
@@ -15,4 +15,26 @@ test("Display advantaged player", () => {
     expect(tennisGame(3, 4)).toBe(advantagePlayer2)
     expect(tennisGame(6,5 )).toBe(advantagePlayer1)
     expect(tennisGame(5, 6)).toBe(advantagePlayer2)
+})
+
+describe("Map score to label", () => {
+    test.each([
+        [0, "Love"],
+        [1, "Fifteen"],
+        [2, "Thirty"],
+        [3, "Forty"],
+    ])("getScoreLabel(%i) -> %s", (score, label) => {
+        expect(getScoreLabel(score)).toBe(label)
+    })
+})
+
+describe("Display score diff based labels", () => {
+    test.each([
+        [1, 0, "Fifteen-Love"],
+        [0, 2, "Love-Thirty"],
+        [3, 1, "Forty-Fifteen"],
+        // just a few representative combos, not all 16
+    ])("(%i, %i) -> %s", (player_1, player_2, label) => {
+        expect(tennisGame(player_1, player_2)).toBe(label)
+    })
 })
